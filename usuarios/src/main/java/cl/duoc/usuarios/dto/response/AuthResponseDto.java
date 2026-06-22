754e0d1a27fc23b7b663cb39289aa369cbb0b4d0
isidora <is.gomezj@duocuc.cl>
2026-06-22 19:03:51 -0400

    Creacion de los DTOs request y response de la autenticacion para obtencion del token

---
+++ usuarios/src/main/java/cl/duoc/usuarios/dto/response/AuthResponseDto.java
@@ -0,0 +1,16 @@
+package cl.duoc.usuarios.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter             @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class AuthResponseDto {
+
+    private String token;
+    private String username;
+    private String rol;
+
+}
